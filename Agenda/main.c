f5d42c197d88a53bd771194c18f279cdb85b83a6
Joao Enrique <joao.bernaldino138@gmail.com>
2024-10-30 16:44:09 -0300

    First Commmit

---
+++ Agenda/main.c
@@ -0,0 +1,32 @@
+#include "TStaticList.h"
+#include "Contato.h"
+#include <stdio.h>
+
+int menu(){
+    int op;
+    printf("1 - Inserir\n2 - Exibir agenda\n0 - Sair");
+    printf("\nSua opcao: ");
+    scanf("%d",&op);
+    return op;
+}
+
+int main(){
+    TStaticList *list = SList_create(10);
+    int op;
+    Contato temp;
+    do{
+        op = menu();
+        switch(op){
+            case 1:
+                temp =  Contato_ui_novo();
+                puts(SList_insert_end(list, temp) ? "Cadastrado com sucesso!" : "Agenda cheia!");
+                break;
+            case 2: 
+                SList_print(list);
+                break;
+            case 0:
+                puts("Bye!");
+        }
+    }while(op != 0);
+    return 0;
+}
